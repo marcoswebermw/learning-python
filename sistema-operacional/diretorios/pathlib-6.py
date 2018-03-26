636a8d8fc59b72f631a52cdc86702c913663e1dd
Marcos Weber <marcoswebermw@gmail.com>
2018-03-26 15:16:23 -0300

    UPDATE sistema-operacional/diretorios/pathlib-6.py

--- sistema-operacional/diretorios/pathlib-6.py
+++ sistema-operacional/diretorios/pathlib-6.py
@@ -5,6 +5,20 @@ from pathlib import Path
 diretorio = Path('/etc')
 
 try:
+    print("GRUPO ------------->")
     [print(x.group()) for x in diretorio.iterdir()]
 except KeyError as erro:
     print('GID do arquivo não encontrado no sistema.')
+
+
+# Retorna o UID(ID do usuário) dono do arquivo.
+# 'KeyError' será lançada, se o UID não for encontrado.
+from pathlib import Path
+
+diretorio = Path('/etc')
+
+try:
+    print("DONO ------------->")
+    [print(x.owner()) for x in diretorio.iterdir()]
+except KeyError as erro:
+    print('UID do arquivo não encontrado no sistema.')
